2d8740dc5c98e2b0424b7514bed8c2f3b5a9b35b
MartynaZulpaite <martyna.zulpaite12@gmail.com>
2024-12-23 00:13:03 +0200

    Additional class created; Char counting added. Struggle encountered - missing 'onClick' in Button description.

--- app/src/main/java/com/example/martechsecondtask/MainActivity.java
+++ app/src/main/java/com/example/martechsecondtask/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.martechsecondtask;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +14,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.martechsecondtask.added.TextCounter;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edUserInput;
+    private TextView tvResult;
+    private Spinner spCountingOpt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Spinner spinner = findViewById(R.id.spCountingOpt);
+
+        spCountingOpt = findViewById(R.id.spCountingOpt);
+        edUserInput = findViewById(R.id.edUserInput);
+        tvResult = findViewById(R.id.tvResult);
+
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.Options,
                 android.R.layout.simple_spinner_item
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        spCountingOpt.setAdapter(adapter);
+    }
+
+    public void btnCalculate(View view) {
+        if (edUserInput != null && tvResult != null) {
+            String userInputPhrase = edUserInput.getText().toString();
+            int result = TextCounter.getCharsCount(userInputPhrase);
+            tvResult.setText(String.valueOf(result));
+        }
     }
 }
---
+++ app/src/main/java/com/example/martechsecondtask/added/TextCounter.java
@@ -0,0 +1,8 @@
+package com.example.martechsecondtask.added;
+
+public class TextCounter {
+
+    public static int getCharsCount(String inputPhrase){
+        return inputPhrase.length();
+    }
+}
